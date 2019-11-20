6071a9df50d977d3c7a8bd32c1d8384386cc03eb
Ryan-Westfall <ryan@reflashed.com>
2019-11-20 00:24:27 -0500

    Removed a now ignored directory

--- client/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import 'draft-js/dist/Draft.css';
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
